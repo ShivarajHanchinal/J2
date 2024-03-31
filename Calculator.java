2045dae49dd4ca85e51163d1d1992de0b7ef420f
ShivarajBHanchinal <rajhanchinal888@gmail.com>
2024-03-31 22:13:12 +0530

    Main branch

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+// Calculator class
+class Calculator {
+    public double squareRoot(double num) {
+        return Math.sqrt(num);
+    }
+
+    public double cubeRoot(double num) {
+        return Math.cbrt(num);
+    }
+}
